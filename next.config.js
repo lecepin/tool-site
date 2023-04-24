18e1b67a5d510582d4db28a88720884da7f259b1
lecepin <383810086@qq.com>
2023-04-24 15:31:33 +0800

    feat:pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+const withPWA = require("next-pwa")({
+  // disable: process.env.NODE_ENV === 'development', // 测试环境禁用
+  sw: "sw.js", // service workder注册入口文件，可选，默认为sw.js
+  dest: "public", // sw.js文件输出目录
+  runtimeCaching: [
+    {
+      urlPattern: /.*/, // 运行时缓存路由
+      handler: "StaleWhileRevalidate", // 运行时缓存策略
+    },
+  ],
+});
+
+module.exports = withPWA(nextConfig);
