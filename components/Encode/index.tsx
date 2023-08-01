34723fbb0e7e8cda17b05ab8a3efeb31131cb934
乐平 <383810086@qq.com>
2023-08-01 09:19:11 +0000

    feat: img to base64

--- components/Encode/index.tsx
+++ components/Encode/index.tsx
@@ -13,13 +13,67 @@ function selectAll(className: string, t = 0) {
       el.select();
       el.focus();
     }, t);
-  } catch (error) {}
+  } catch (error) { }
 }
 
 export default function Qr() {
   const [key, setKey] = useState("Base64");
   const [inputValue, setInputValue] = useState("");
   const [outputValue, setOutputValue] = useState("");
+  const [outputImgValue, setOutputImgValue] = useState("");
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+
+    setKey(urlParams.get("s") || "Base64");
+  }, []);
+
+  useEffect(() => {
+    function eventHandler(e: ClipboardEvent) {
+      const items = e.clipboardData?.items;
+
+      if (!items) {
+        return;
+      }
+
+      for (let i = 0; i < items.length; i++) {
+        if (items[i].type.indexOf("image") !== -1) {
+          const file = items[i].getAsFile();
+
+          if (file) {
+            handleImg2Base64File(file)
+          }
+
+          break;
+        }
+      }
+    }
+
+    if (key == "Base64img") { document.addEventListener("paste", eventHandler); }
+
+    return () => {
+      document.removeEventListener("paste", eventHandler);
+    };
+  }, [key]);
+
+  const handleImg2Base64File = (file: File) => {
+    const reader = new FileReader();
+    reader.onload = function (event) {
+      const url = (event.target?.result as string) || '';
+      setOutputImgValue(url)
+      selectAll("." + styles.outputImg64);
+
+
+      const imgEl = document.querySelector('.' + styles.outputImgPreview) as HTMLImageElement
+
+      if (imgEl) {
+        imgEl.src = url
+        imgEl.style.display = 'block'
+      }
+    };
+
+    reader.readAsDataURL(file);
+  }
 
   return (
     <div>
@@ -87,6 +141,41 @@ export default function Qr() {
                     setOutputValue(e.target.value);
                   }}
                 ></Input.TextArea>
+              </div>
+            ),
+          }, {
+            key: "Base64img",
+            label: `图片 Base64`,
+            children: (
+              <div>
+                <Upload.Dragger
+                  className={styles.upload_list}
+                  multiple={false}
+                  maxCount={1}
+                  accept="image/png, image/jpeg, image/gif, image/bmp"
+                  beforeUpload={(e) => {
+                    if (e) {
+                      handleImg2Base64File(e)
+                    }
+                    return false;
+                  }}
+                >
+                  <p className="ant-upload-drag-icon">
+                    <InboxOutlined />
+                  </p>
+                  <p>点击 或者 拖动图片到虚线框内 或者 Ctrl+V</p>
+                </Upload.Dragger>
+                <Input.TextArea
+                  rows={4}
+                  placeholder="Base64 编码或解码的结果"
+                  className={styles.outputImg64}
+                  value={outputImgValue}
+                  onChange={(e) => {
+                    setOutputImgValue(e.target.value);
+                  }}
+                ></Input.TextArea>
+
+                <img className={styles.outputImgPreview} />
               </div>
             ),
           },
