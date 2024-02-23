4e274de59a19bd00db33e0c88e86efec34e643dc
乐平 <383810086@qq.com>
2024-02-24 01:45:24 +0800

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="zh">
-      <Head />
+      <Head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5223225078583939"
+     crossorigin="anonymous"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
